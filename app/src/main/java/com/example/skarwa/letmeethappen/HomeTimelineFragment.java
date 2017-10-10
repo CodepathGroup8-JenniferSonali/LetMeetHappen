1b4ea28a27639e2c7ae9bc35f9913d2b19fbf32b
Jennifer Godinez <jennifer.p1210@gmail.com>
2017-10-09 17:42:11 -0700

    initial framework

---
+++ app/src/main/java/com/example/skarwa/letmeethappen/HomeTimelineFragment.java
@@ -0,0 +1,24 @@
+package com.example.skarwa.letmeethappen;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by jennifergodinez on 10/2/17.
+ */
+
+public class HomeTimelineFragment extends EventsListFragment {
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //populateTimeline();
+
+    }
+
+
+    private void populateTimeline () {
+    }
+
+
+}
